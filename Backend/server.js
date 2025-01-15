e3de7863d53a5a41f1f07844467580bb79f3959f
Student Application <saihemanth2267@gmail.com>
2025-01-15 11:59:29 -0500

    Improvizing the put end point

--- Backend/server.js
+++ Backend/server.js
@@ -45,13 +45,18 @@ app.put("/api/products/id:", async (req, res) => {
     const {id} = req.params;
     const Product = req.body;
 
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid Product Id"})
+    }
+
     try {
-      const updatedProduct =  await Product.findByIdAndUpdate(id, product, {new: true});
+      const updatedProduct =  await Product.findByIdAndUpdate(id, Product, {new: true});
       res.status(200).json({success: true, data: updatedProduct});
     } catch (error) {
-    res.status(500).json{false, message: "Product not found"}
+    res.status(500).json({success: false, message: "Product not found"});
     }
-);
+});
+
 app.delete ("/api/products/:id", async (req, res) => {
     const { id } = req.params;
 try{
