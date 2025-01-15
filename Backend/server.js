16109ee7e8a0fc0c382b6bfb62bccbc89fcb825c
Student Application <saihemanth2267@gmail.com>
2025-01-14 19:39:54 -0500

    end point to update a product

--- Backend/server.js
+++ Backend/server.js
@@ -41,12 +41,24 @@ app.post("/api/products", async (req, res) => { // Add `async` to use `await` in
     }
 });
 
+app.put("/api/products/id:", async (req, res) => {
+    const {id} = req.params;
+    const Product = req.body;
+
+    try {
+      const updatedProduct =  await Product.findByIdAndUpdate(id, product, {new: true});
+      res.status(200).json({success: true, data: updatedProduct});
+    } catch (error) {
+    res.status(500).json{false, message: "Product not found"}
+    }
+);
 app.delete ("/api/products/:id", async (req, res) => {
     const { id } = req.params;
 try{
     await Product.findByIdAndDelete(id);
     res.status(200).json({success: true, message: "Product Deleted"})
 } catch(error) {
+    console.log("Error in deleting product:", error.message);
     res.status(404).json({success: false, message: "Product not found"});
 }
 
